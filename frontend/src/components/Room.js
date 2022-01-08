dca872486df2c3533cbd8552c5bcaff21ea34247
Jim97M <wabuyajames@gmail.com>
2022-01-08 10:24:02 +0300

    Update Settings

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -7,10 +7,12 @@ export default class Room extends Component {
       votesToSkip: 2,
       guestCanPause: false,
       isHost: false,
+      showSettings: false,
     };
     this.roomCode = this.props.match.params.roomCode;
     this.getRoomDetails();
     this.leaveRoomRequest = this.leaveRoomRequest.bind(this);
+    this.updateShowSettings = this.updateShowSettings.bind(this);
   }
 
   getRoomDetails() {
@@ -41,6 +43,25 @@ export default class Room extends Component {
          this.props.leaveRoomCallback();
          this.props.history.push("/");
        });
+  }
+
+  updateShowSettings(value){
+    this.setState({
+      showSettings: value,
+    })
+  }
+
+  renderUpdateButton(){
+    return(
+      <Grid item xs={12} align="center">
+        <Button 
+          variant="contained"
+          color="primary"
+          onClick={() => this.updateShowSettings(true)}>
+            Settings
+          </Button>
+      </Grid>
+    );
   }
 
   render() {
