2bc32e471b0950d761238b6bd9fd181cefcc1430
Jim97M <wabuyajames@gmail.com>
2022-02-18 01:09:06 +0300

    Activate Component

---
+++ frontend/src/containers/Activate.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+import {Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import activate from '../redux/actions/auth';
+
+const Activation = () => {
+    
+}
